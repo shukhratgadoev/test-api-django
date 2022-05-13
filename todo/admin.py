753f1c62754ca4e51bed5fa157faeb38a3793540
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-05-13 11:52:57 +0500

    feat: finish next step api

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'date', 'done')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title')
+    list_editable = ('done',)
+    list_filter = ('done',)
+    
+    
+admin.site.register(Todo, TodoAdmin)        
